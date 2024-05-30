27ffed70d5a4b53c5c84474d8d0e8fcfb2a8c048
Pablo Mengibar <pabmengibar@gmail.com>
2024-05-30 19:09:45 +0000

    tasks

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const validUrl = require('valid-url');
+const shortid = require('shortid');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const originalUrls = {}; // Almacena las URLs originales
+const shortUrls = {}; // Almacena las URLs acortadas
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +25,41 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// Endpoint para acortar URLs
+app.post('/api/shorturl', function(req, res) {
+  const { url } = req.body;
+
+  // Verifica si la URL es válida
+  if (!validUrl.isUri(url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Genera un ID corto único
+  const shortId = shortid.generate();
+
+  // Almacena la URL original y la URL corta en los objetos correspondientes
+  originalUrls[shortId] = url;
+  shortUrls[url] = shortId;
+
+  // Retorna la URL original y la URL corta
+  res.json({ original_url: url, short_url: shortId });
+});
+
+// Endpoint para redirigir a la URL original
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const { short_url } = req.params;
+
+  // Verifica si el ID corto existe
+  if (!originalUrls.hasOwnProperty(short_url)) {
+    return res.json({ error: 'invalid short url' });
+  }
+
+  // Obtiene la URL original y redirige al usuario
+  const originalUrl = originalUrls[short_url];
+  res.redirect(originalUrl);
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
